f4040d2e42cb2a8b5c5806e8de9a34976520cfe5
Steve Horlback <stevehorlback@Steves-MacBook-Pro.local>
2017-08-22 15:14:39 -0400

    used mustache views

--- express-app.js
+++ express-app.js
@@ -1,29 +1,60 @@
+const mustacheExpress = require('mustache-express');
 const express = require('express');
+const data = require('./data');
+
 const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+app.use(express.static("public"))
+
+app.get('/', function (req, res) {
+  res.render('index.mustache', {users: data.users });
+});
+
+app.listen(3001, function () {
+  console.log('coding helps solve problems!')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //changing roboHunt into users
 
-app.listen(3000, function () {
-console.log(roboHunt);
-const body = document.querySelector("body");
-for (i = 0; i < roboHunt.users.length; i++) {
-header += `
- <div>
- <p><img src= "${roboHunt.users[i].avatar}" ></img></p>
-  <p id = "name">${roboHunt.users[i].name} </p>
-  <p id = "job">${roboHunt.users[i].job}</p>
-  <p id = "street">${roboHunt.users[i].company}</p>
+// app.listen(3000, function () {
+// const body = document.querySelector("body");
+// for (i = 0; i < roboHunt.users.length; i++) {
+// header += `
+//  <div>
+//  <p><img src= "${roboHunt.users[i].avatar}" ></img></p>
+//   <p id = "name">${roboHunt.users[i].name} </p>
+//   <p id = "job">${roboHunt.users[i].job}</p>
+//   <p id = "street">${roboHunt.users[i].company}</p>
+//
+//
+//
+//   </div>`;
+// }
 
 
+ // body.innerHTML = header;
 
-  </div>`;
-}
-
-
- body.innerHTML = header;
-
-})
+// })
 
 
  // app.get('/', function (req, res) {
